3b21ab03a158a93d8ce4ed07b55ecad43ec25359
Ben Kim <benkim1028@gmail.com>
2018-05-23 09:30:07 -0700

    Second Attempt (HashMap instead of ArrayList)
    2 times faster

--- leetcode/Longest Substring Without Repeating Characters.java
+++ leetcode/Longest Substring Without Repeating Characters.java
@@ -2,14 +2,14 @@ class Solution {
     public int lengthOfLongestSubstring(String s) {
         char[] chars = s.toCharArray();
         int length = chars.length;
-        List<Character> temp;
+        Map<Character, Boolean> temp;
         int answer = 0;
         for (int i = 0 ; i < length ; i ++){
-            temp  = new ArrayList<Character>();
-            temp.add(chars[i]);
+            temp  = new HashMap<Character, Boolean>();
+            temp.put(chars[i], true);
             for (int j = i + 1 ; j < length ; j ++){
-                if(temp.contains(chars[j])) break;
-                else temp.add(chars[j]);
+                if(temp.get(chars[j]) != null) break;
+                else temp.put(chars[j], true);
             }
             if(temp.size() > answer) answer = temp.size();
         }
