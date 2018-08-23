9cd139bcc39dc9878513e74a578f0585ff24d3ee
Ben Kim <benkim1028@gmail.com>
2018-08-22 21:49:30 -0700

    second attempt (86.91% beated)

--- leetcode/Palindrome Number.java
+++ leetcode/Palindrome Number.java
@@ -12,3 +12,17 @@ class Solution {
         return true;
     }
 }
+
+class Solution {
+    public boolean isPalindrome(int x) {
+        if(x < 0) return false;
+        if(x < 10) return true;
+        int original = x;
+        int sum = 0;
+        while(x > 0){
+            sum = sum * 10 + x % 10;
+            x = x / 10;
+        }
+        return original == sum;
+    }
+}
