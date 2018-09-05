0dad3b3409dbbead06cbef991f0816ae927d7eac
Ben Kim <benkim1028@gmail.com>
2018-09-05 12:14:29 -0700

    Cleaner

--- leetcode/Climbing Stairs.java
+++ leetcode/Climbing Stairs.java
@@ -4,14 +4,8 @@ class Solution {
         arr.add(0, 0);
         arr.add(1, 1);
         arr.add(2, 2);
-        helper(arr, n);
-        return arr.get(n);
-        
-    }
-    private void helper (ArrayList<Integer> arr, int n){
-        if (n < 3) return;
         for(int start = 3; start <= n; start++){
             arr.add(start, new Integer(arr.get(start-1) + arr.get(start-2)));
         }
-    }
+        return arr.get(n);
 }
