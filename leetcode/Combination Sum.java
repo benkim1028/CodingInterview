ec72d6a910c8e5c3727a8df67520d9525e7d7e45
Ben Kim <benkim1028@gmail.com>
2018-10-10 22:17:49 -0700

    Create Combination Sum.java

---
+++ leetcode/Combination Sum.java
@@ -0,0 +1,21 @@
+class Solution {
+    public List<List<Integer>> combinationSum(int[] candidates, int target) {
+        List<List<Integer>> ans = new ArrayList<>();
+        Arrays.sort(candidates);
+        findAllCombinations(ans, new ArrayList<Integer>(), candidates, target, 0, 0);
+        return ans;
+    }
+    
+    private void findAllCombinations(List<List<Integer>> ans, List<Integer> com, int[] candidates, int target, int sum, int index){
+        if(sum > target) return;
+        if(sum == target) {
+            ans.add(new ArrayList<Integer>(com));
+            return;    
+        };
+        for(int i = index; i < candidates.length; i++){
+            com.add(candidates[i]);
+            findAllCombinations(ans, com, candidates, target, sum + candidates[i], i);
+            com.remove(com.size() -1 );
+        }
+    }
+}
