55aeb6382bb7ab4e1a2f8e07d5529a0a57d5a179
Ben Kim <benkim1028@gmail.com>
2018-10-17 22:47:23 -0700

    Beat 73%

--- leetcode/Merge Two Binary Trees.java
+++ leetcode/Merge Two Binary Trees.java
@@ -9,9 +9,6 @@
  */
 class Solution {
     public TreeNode mergeTrees(TreeNode t1, TreeNode t2) {
-        return merge(t1, t2);
-    }
-    private TreeNode merge(TreeNode t1, TreeNode t2){
         if(t1 == null && t2 == null) return null;
         TreeNode newNode;
         if(t1 == null) newNode = t2;
@@ -19,8 +16,8 @@ class Solution {
         else{
             newNode = t1;
             newNode.val += t2.val;
-            newNode.left = merge(t1.left, t2.left);
-            newNode.right = merge(t1.right, t2.right);
+            newNode.left = mergeTrees(t1.left, t2.left);
+            newNode.right = mergeTrees(t1.right, t2.right);
         }
         return newNode;
     }
